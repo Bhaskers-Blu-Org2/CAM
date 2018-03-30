e683a076e26997f28eb661e8961185d906bd97f5
Jacob Marquez <marcusfenixdubstep@gmail.com>
2018-03-29 21:00:53 -0700

    Update CAM.tests.ps1

--- CAM.tests.ps1
+++ CAM.tests.ps1
@@ -9,6 +9,7 @@ if ($CloudBuild)
     write-host $env:TenantId
     write-host $env:KeyVault
     $Json = Get-Content -Raw -Path "$((Get-Item -Path ".\").FullName)\CAMConfig.json" | ConvertFrom-Json
+    write-host $Json
     $Json.AADApplicationId = $env:AADApplicationId.ToString()
     $Json.TenantId = $env:TenantId.ToString()
     $Json.AADApplicationKey = $env:AADApplicationKey.ToString()
