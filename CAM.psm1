29da2ec061641f640b7200c3016efeefeb3ebe70
Jacob Marquez <marcusfenixdubstep@gmail.com>
2018-05-10 14:10:59 -0700

    Updated Help comments

--- CAM.psm1
+++ CAM.psm1
@@ -1,4 +1,4 @@
-﻿# CONSTRUCTORS
+# CONSTRUCTORS
 
 <# 
 .SYNOPSIS
@@ -266,6 +266,8 @@ param(
     Authenticate to AAD Application using a encrypted key that has been whitelisted with an applicable service principal.
 .PARAMETER CAMConfig
     (optional) A configuration object used to override the fallback variable and any present configuration files.
+.PARAMETER Key
+    (optional) An AAD application key to be used if it differs from the one being used in the CAMConfig.
 .EXAMPLE
     C:\PS> Authenticate-WithKey -CAMConfig $CustomConfig
 #>
@@ -274,10 +276,10 @@ param(
     [parameter()]
     [PSTypeName("CAMConfig")]$CAMConfig = $script:CAMConfig,
     [parameter()]
-    [SecureString]$key = $CAMConfig.AADApplicationKey
+    [SecureString]$Key = $CAMConfig.AADApplicationKey
 )
     try {
-        $Credential = New-Object System.Management.Automation.PSCredential($CAMConfig.AADApplicationID, $key)
+        $Credential = New-Object System.Management.Automation.PSCredential($CAMConfig.AADApplicationID, $Key)
         Login-AzureRmAccount -Credential $Credential -Tenant $CAMConfig.TenantId -ServicePrincipal -ErrorAction Stop
     }
     catch {
@@ -324,6 +326,8 @@ param(
     Iterate through the secrets and download or delete the certificate as specified in the certificateVersion "deploy" property. Repeat this process for the certificate section.
 .PARAMETER LocalManifest
     (optional) The full path to a valid json manifest file to be used in place of a passed in object or manifest available in the KeyVault.
+.PARAMETER Manifest
+    (optional) A PSCustom object containing the manifest information to override a local or keyvault sourced manifest.
 .PARAMETER CAMConfig
     (optional) A configuration object used to override the fallback variable and any present configuration files.
 .EXAMPLE
@@ -484,17 +488,21 @@ param(
 
 <#
 .SYNOPSIS
-	This function will download and install a certificate object from a key vault and install it on local machine.
+    This function will download and install a certificate object from a key vault and install it on local machine.
 .DESCRIPTION
     This script runs through key vault commands to download a certificate object from a key vault and install it locally.
 .PARAMETER CertName
-    secret name in key vault
+    Secret name in key vault.
 .PARAMETER CertVersion
     (optional) Version GUID of the secret you want to retrieve.
+.PARAMETER Export
+    (optional) Path to folder where the public key should be exported as .cert file.
 .PARAMETER CertStoreName
     (optional) Certificate Store Name that you would like the certificate installed to. Defaults to "My"
 .PARAMETER CertStoreLocation
     (optional) Certificate Store Location that you would like the certificate installed to. Defaults to "LocalMachine"
+.PARAMETER KeyStorageFlags
+    (optional) Key storage flags to be used when the certificate is imported to the store. Defaults to "PersistKeySet"
 .PARAMETER CAMConfig
     (optional) A configuration object used to override the fallback variable and any present configuration files.
 .EXAMPLE
@@ -561,10 +569,16 @@ param(
     Cert name in key vault
 .PARAMETER CertVersion
     (optional) Version GUID of the secret you want to retrieve.
+.PARAMETER Export
+    (optional) Path to folder where the public key should be exported as .cert file.
 .PARAMETER CertStoreName
     (optional) Certificate Store Name that you would like the certificate installed to. Defaults to "My"
 .PARAMETER CertStoreLocation
     (optional) Certificate Store Location that you would like the certificate installed to. Defaults to "LocalMachine"
+.PARAMETER KeyStorageFlags
+    (optional) Key storage flags to be used when the certificate is imported to the store. Defaults to "PersistKeySet"
+.PARAMETER Unstructured
+    If true, will download the secret without disassembling it as a JSON object, and import with no password. Defaults to "false"
 .PARAMETER CAMConfig
     (optional) A configuration object used to override the fallback variable and any present configuration files.
 .EXAMPLE
@@ -796,6 +810,8 @@ param(
     The friendly name of the certificate you would like to remove.
 .PARAMETER CertVersion
     (optional) Version GUID of the secret you want to retrieve.
+.PARAMETER Unstructured
+    If true, will download the secret without disassembling it as a JSON object, and import with no password. Defaults to "false"
 .PARAMETER CAMConfig
     (optional) A configuration object used to override the fallback variable and any present configuration files.
 .EXAMPLE
